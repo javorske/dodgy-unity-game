d15371a4426adc1b163589e1110f7db01ed4f81f
javorske <jaywors@gmail.com>
2021-08-01 14:07:37 +0200

    created-players-mover-script
    Created script Mover and attached it to player object

---
+++ Dodgy/Assets/Scripts/Mover.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
