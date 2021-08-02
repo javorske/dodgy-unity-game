b7d67c638869a808bed16e167a513aec9319638a
javorske <jaywors@gmail.com>
2021-08-02 12:18:59 +0200

    added-code-to-players-movement

--- Dodgy/Assets/Scripts/Mover.cs
+++ Dodgy/Assets/Scripts/Mover.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
+    [SerializeField] float moveSpeed;
     void Start()
     {
         
@@ -11,6 +12,9 @@ public class Mover : MonoBehaviour
 
     void Update()
     {
-        
+        float xValue = Input.GetAxis("Horizontal") * Time.deltaTime;
+        float zValue = Input.GetAxis("Vertical") * Time.deltaTime;
+
+        transform.Translate(xValue * moveSpeed, 0, zValue * moveSpeed);
     }
 }
