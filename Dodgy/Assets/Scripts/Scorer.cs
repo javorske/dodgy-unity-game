481418afdfdf69d90fc5c373f6b0feeec6ffde8d
javorske <jaywors@gmail.com>
2021-08-03 16:55:16 +0200

    Update Scorer.cs
    Added code to handle counting of collisions with other objects

--- Dodgy/Assets/Scripts/Scorer.cs
+++ Dodgy/Assets/Scripts/Scorer.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class Scorer : MonoBehaviour
 {
+    int hits = 0;
     private void OnCollisionEnter(Collision collision)
     {
-        
+        if (collision.gameObject.name != "Plane")
+        {
+            hits++;
+            Debug.Log("Bumped into things " + hits + " times");
+        }
     }
 }
