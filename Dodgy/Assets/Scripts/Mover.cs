9c04555bdde3c020e9049cda428978600c6edb4c
javorske <jaywors@gmail.com>
2021-08-03 16:08:21 +0200

    Refactored Mover.cs
    Created method MovePlayer and moved code there from Update. MovePlayer is now called in Update

--- Dodgy/Assets/Scripts/Mover.cs
+++ Dodgy/Assets/Scripts/Mover.cs
@@ -7,14 +7,17 @@ public class Mover : MonoBehaviour
     [SerializeField] float moveSpeed;
     void Start()
     {
-        
+
     }
 
     void Update()
     {
+        MovePlayer();
+    }
+    void MovePlayer()
+    {
         float xValue = Input.GetAxis("Horizontal") * Time.deltaTime;
         float zValue = Input.GetAxis("Vertical") * Time.deltaTime;
-
         transform.Translate(xValue * moveSpeed, 0, zValue * moveSpeed);
     }
 }
