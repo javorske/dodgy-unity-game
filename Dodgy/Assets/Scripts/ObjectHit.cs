a5acff161ea0301ee14886fed5ea3eeb49aa85a2
javorske <jaywors@gmail.com>
2021-08-05 13:30:10 +0200

    Update ObjectHit.cs
    Tagged Player object with "Player" tag.
    Added If statement to script so now it changes color of Obstacle only when it's hit by object with Player tag.

--- Dodgy/Assets/Scripts/ObjectHit.cs
+++ Dodgy/Assets/Scripts/ObjectHit.cs
@@ -6,6 +6,9 @@ public class ObjectHit : MonoBehaviour
 {
     private void OnCollisionEnter(Collision collision)
     {
-        GetComponent<MeshRenderer>().material.color = Color.red;
+        if (collision.gameObject.tag == "Player")
+        {
+            GetComponent<MeshRenderer>().material.color = Color.red;
+        }
     }
 }
