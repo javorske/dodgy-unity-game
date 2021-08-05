860f28753eda14d7bb3e7466d8ef65f4429c02b0
javorske <jaywors@gmail.com>
2021-08-05 12:59:40 +0200

    Update Dropper.cs
    Added code to Dropper.cs so that object to which this script is attached to will turn off it's MeshRenderer and Rigidbody's UseGravity to false, and after certain amount of time sets it to true

--- Dodgy/Assets/Scripts/Dropper.cs
+++ Dodgy/Assets/Scripts/Dropper.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class Dropper : MonoBehaviour
 {
-    // Start is called before the first frame update
+    MeshRenderer renderer;
+    Rigidbody rigidbody;
+    [SerializeField] float timeToWait = 5f;
     void Start()
     {
-        
+        renderer = GetComponent<MeshRenderer>();
+        rigidbody = GetComponent<Rigidbody>();
+
+        renderer.enabled = false;
+        rigidbody.useGravity = false;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Time.time > timeToWait)
+        {
+            renderer.enabled = true;
+            rigidbody.useGravity = true;
+        }
     }
 }
